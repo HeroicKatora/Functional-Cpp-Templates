02e619c7ed3791175f85fb7e33d02e4f26739b90
HeroicKatora <andreas.molzer@gmx.de>
2017-05-04 16:39:04 +0200

    Whitespace change on list

--- headerlib/hdr/types/list.hpp
+++ headerlib/hdr/types/list.hpp
@@ -12,63 +12,63 @@
 using namespace hdrstd;
 
 namespace hdr::list {
-	/** List a = Nil | Cons a (List a)
-	*/
-	struct Nil;
+/** List a = Nil | Cons a (List a)
+*/
+struct Nil;
 
-	template<typename head, typename tail>
-	struct Cons;
+template<typename head, typename tail>
+struct Cons;
 
-	/** []
-	*/
-	using EmptyList = Nil;
+/** []
+*/
+using EmptyList = Nil;
 
-	template<typename L>
-	struct Head;
-	using head = ::hdr::std::TypeFunction<Head>;
-	template<typename L>
-	struct Last;
-	using last = ::hdr::std::TypeFunction<Last>;
-	template<typename L>
-	struct Tail;
-	using tail = ::hdr::std::TypeFunction<Tail>;
-	template<typename L>
-	struct Init;
-	using init = ::hdr::std::TypeFunction<Init>;
+template<typename L>
+struct Head;
+using head = ::hdr::std::TypeFunction<Head>;
+template<typename L>
+struct Last;
+using last = ::hdr::std::TypeFunction<Last>;
+template<typename L>
+struct Tail;
+using tail = ::hdr::std::TypeFunction<Tail>;
+template<typename L>
+struct Init;
+using init = ::hdr::std::TypeFunction<Init>;
 
-	// Head []
-	template<>
-	struct Head<EmptyList>; // exception?
-	// Head (head:tail)
-	template<typename head, typename tail>
-	struct Head<Cons<head, tail>> { using type = head; };
+// Head []
+template<>
+struct Head<EmptyList>; // exception?
+// Head (head:tail)
+template<typename head, typename tail>
+struct Head<Cons<head, tail>> { using type = head; };
 
-	// Last []
-	template<>
-	struct Last<EmptyList>; // exception?
-	// Last (head:[])
-	template<typename head>
-	struct Last<Cons<head, EmptyList>> { using type = head; };
-	// Last (head:tail)
-	template<typename head, typename tail>
-	struct Last<Cons<head, tail>> { using type = Last<tail>; };
+// Last []
+template<>
+struct Last<EmptyList>; // exception?
+// Last (head:[])
+template<typename head>
+struct Last<Cons<head, EmptyList>> { using type = head; };
+// Last (head:tail)
+template<typename head, typename tail>
+struct Last<Cons<head, tail>> { using type = Last<tail>; };
 
-	// Tail []
-	template<>
-	struct Tail<EmptyList>; // exception?
-	// Tail (head:tail)
-	template<typename head, typename tail>
-	struct Tail<Cons<head, tail>> { using type = tail; };
+// Tail []
+template<>
+struct Tail<EmptyList>; // exception?
+// Tail (head:tail)
+template<typename head, typename tail>
+struct Tail<Cons<head, tail>> { using type = tail; };
 
-	// Init []
-	template<>
-	struct Init<EmptyList>; // exception?
-	// Init (head:[])
-	template<typename head>
-	struct Init<Cons<head, EmptyList>> { using type = EmptyList; };
-	// Last (head:tail)
-	template<typename head, typename tail>
-	struct Init<Cons<head, tail>> { using type = Cons<head, Init<tail>>; };
+// Init []
+template<>
+struct Init<EmptyList>; // exception?
+// Init (head:[])
+template<typename head>
+struct Init<Cons<head, EmptyList>> { using type = EmptyList; };
+// Last (head:tail)
+template<typename head, typename tail>
+struct Init<Cons<head, tail>> { using type = Cons<head, Init<tail>>; };
 
 }
 
